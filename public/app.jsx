72d7ba60fcf1b613e24e2fc59d195df9047d89b0
Alex Shook <alexandra.shook@gmail.com>
2017-03-10 17:41:26 -0500

    Update with components and props

--- public/app.jsx
+++ public/app.jsx
@@ -1,16 +1,29 @@
 var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'This is from the component'
+    };
+  },
+
   render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+
     return (
       <div>
-        <h1>Hello React!</h1>
-        <p>This is from the component.</p>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
       </div>
     );
   }
 });
 
+var firstName = "Alex";
+var ohHey = "Oh hey";
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message={ohHey} />,
   document.getElementById('app')
 );
 
